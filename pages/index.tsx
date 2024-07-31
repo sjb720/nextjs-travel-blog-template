ce2fcbb4efb41f20edf98800a5125f4fe7e07b42
Stephen Buonamassa <sbuonama@umd.edu>
2024-07-31 17:07:36 -0600

    updated favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <title>Travel Blog</title>
         <meta name="description" content="Who Asked? Travel blog!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className="trip-grid">
         {Object.keys(TRIPS).map(key =>{
